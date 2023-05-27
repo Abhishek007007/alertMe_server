958776ffb6a0095d3e39e7aca639cc6daee31816
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2023-05-27 22:21:48 +0530

    feat: added register endpoint

---
+++ controllers/register.js
@@ -0,0 +1,60 @@
+const User = require("../model/user");
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+require("dotenv").config();
+
+// Register
+const registerUser = async (req, res, next) => {
+  // our register logic goes here...
+  // Our register logic starts here
+  try {
+    // Get user input
+    const { user_name, email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password && user_name)) {
+      return res.status(400).send({
+        code: 400,
+        status: "All input is required",
+        required: "user_name, email, password",
+      });
+    }
+
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+    
+    //Encrypt user password
+    encryptedPassword = await bcrypt.hash(password, 10);
+
+    // Create user in our database
+    const user = await User.create({
+      user_name,
+      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      password: encryptedPassword,
+    });
+
+    // Create token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+    // save user token
+    user.token = token;
+
+    // return new user
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = { registerUser };
--- src/app.js
+++ src/app.js
@@ -2,11 +2,17 @@
 const express = require('express');
 require("dotenv").config();
 require("../config/database").connect();
+const registerRouter = require("../routes/register");
 
 const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send({"error" : "api is available at /api/v1"});
+  });
+
+app.use("/api/v1/register", registerRouter);
 // Logic goes here
 
 module.exports = app;
